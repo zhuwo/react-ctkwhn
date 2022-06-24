45877f452540dc8b416bc634f53ce6ba11eb0c1a
zhuwo <zhuwo_jian@163.com>
2022-06-24 15:03:22 +0800

    1 file modified

--- index.tsx
+++ index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
 import Demo from './demo';
 
-ReactDOM.createRoot(document.querySelector('#root')).render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <Demo />
-    </StyledEngineProvider>
-  </React.StrictMode>
-);
+const rootEle = document.querySelector('#root');
+if (rootEle) {
+  ReactDOM.createRoot(rootEle).render(
+    <React.StrictMode>
+      <StyledEngineProvider injectFirst>
+        <Demo />
+      </StyledEngineProvider>
+    </React.StrictMode>
+  );
+}
